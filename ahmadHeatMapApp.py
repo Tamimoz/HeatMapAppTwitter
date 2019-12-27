662c6b150e88493fe1f85de5f671652687e8524c
Ahmad Tamimi <tamimoz@udel.edu>
2019-12-27 09:21:50 -0500

    Collecting tweets based on certain keywords and after a certain date is implemented

--- ahmadHeatMapApp.py
+++ ahmadHeatMapApp.py
@@ -15,3 +15,20 @@ api = tw.API(auth, wait_on_rate_limit=True)
 
 # Post a tweet from Python (commented out)
 # api.update_status("Look, I'm tweeting from #Python using the twitter API!")
+
+#Define search term and date since as variables
+search_word = "#wildfires"
+date_since = "2018-11-10"
+
+#Defining most_recent_count (number of mmost recent tweets to display)
+most_recent_count = 10
+
+#Collecting tweets, using .Cursor(), that contain the specified search terms 
+tweets = tw.Cursor(api.search,
+              q=search_word,
+              lang="en",
+              since=date_since).items(most_recent_count)
+              
+#Iterate over and print tweets 
+for tweet in tweets:
+    print(tweet.text)
